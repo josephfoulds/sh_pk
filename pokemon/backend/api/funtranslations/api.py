9e7e6e3f651f5182a43398413233f15f417b0ed2
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-12 11:47:52 +0100

    [API] Add FunTranslation authentication and standardize output

--- pokemon/backend/api/funtranslations/api.py
+++ pokemon/backend/api/funtranslations/api.py
@@ -1,5 +1,10 @@
 import requests
+import os
 
 def translate(text):
-    r = requests.post('https://api.funtranslations.com/translate/shakespeare.json', data={"text":text})
+    headers = {
+        "X-FunTranslations-Api-Secret": os.getenv('FT_API')
+    }
+    
+    r = requests.post('https://api.funtranslations.com/translate/shakespeare.json', data={"text":text}, headers=headers)
     return(r.json()["contents"]["translated"])
--- pokemon/backend/views.py
+++ pokemon/backend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from backend.api.pokeapi.api import get_species
 from backend.api.funtranslations.api import translate
@@ -15,4 +15,4 @@ class Pokemon(View):
 
         description = get_species(name)
         translation = translate(description)
-        return HttpResponse(translation)
+        return JsonResponse({"name": name, "description":translation})
