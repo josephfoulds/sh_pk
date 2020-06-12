341fceb35adb0060e55ab0044afe4b56e47d6d3c
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-12 10:47:33 +0100

    [Housekeeping] Initialize React Web-App

--- pokemon/pokemon/settings.py
+++ pokemon/pokemon/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pokemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'pokemon/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,6 +113,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'pokemon/static'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
