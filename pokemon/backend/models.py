c543a719d69b7abc69eb5c1ee8142f0f17aeb517
Joseph Foulds <josephfoulds@googlemail.com>
2020-06-19 09:17:17 +0100

    [Backend] Add SQLite caching with 24h period

--- pokemon/backend/models.py
+++ pokemon/backend/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    creation = models.DateTimeField(auto_now_add=True)
--- pokemon/backend/views.py
+++ pokemon/backend/views.py
@@ -1,9 +1,13 @@
+import datetime
+
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 
 from backend.api.pokeapi.api import get_species
 from backend.api.funtranslations.api import translate
+
+from backend.models import Pokemon as PokemonModel
 
 # /backend/
 def index(request):
@@ -19,6 +23,21 @@ class Pokemon(View):
         if not name:
             return HttpResponse(status=400)
 
+        # nb: Django will throw a DoesNotExist exception on cache miss
+        try:
+            # Retrieve from local cache
+            pokemon = PokemonModel.objects.get(name=name)
+            # Determine whether we should lazy-evict after 24h
+            elapsed = datetime.datetime.now(datetime.timezone.utc) - pokemon.creation
+            if (elapsed.total_seconds() >= 60*60*24):
+                # Evict the cache
+                pokemon.delete()
+            else:
+                return JsonResponse({"name": pokemon.name, "description":pokemon.description})
+        except Exception:
+            # Add some fancy error logging in here, but likely will happen on cache miss
+            pass
+
         # Retrieve the species information from the pokeapi
         (response, description) = get_species(name)
 
@@ -32,6 +51,10 @@ class Pokemon(View):
         # Return through error response code if not HTTP200
         if (response != 200):
             return HttpResponse(status=response)
+
+        # Populate the cache
+        pokemon = PokemonModel(name=name, description=translation)
+        pokemon.save()
 
         # Package the information into a JSON and return it to the client
         return JsonResponse({"name": name, "description":translation})
